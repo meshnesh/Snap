d87b4b5808b1798364ce57583be32c09a7a57563
meshnesh <tonnie.nesh@gmail.com>
2016-03-21 12:21:08 +0300

    starting app

---
+++ app/src/main/java/com/example/tune/snap/snap.java
@@ -0,0 +1,13 @@
+package com.example.tune.snap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class snap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snap);
+    }
+}
